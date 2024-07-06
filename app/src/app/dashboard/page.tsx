f1e3168ce3358e233c66465a0413ee8bedf853f4
Leonardo Grimaldi <leonardo.grimaldi2@studio.unibo.it>
2024-07-06 16:25:58 +0200

    started navbar

---
+++ app/src/app/dashboard/page.tsx
@@ -0,0 +1,19 @@
+export default function Page() {
+    return (
+        <nav className="flex flex-col justify-start h-full">
+            <div>
+                DB-Prigione
+            </div>
+            <div>
+                Home
+            </div>
+            <div>
+                Detenuti
+            </div>
+            <div>
+                Guardie
+            </div>
+        </nav>
+        
+    );
+  }
