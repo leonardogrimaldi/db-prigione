1adec36ef9b9941b6689da98dbf27b3663e29e84
Leonardo Grimaldi <leonardo.grimaldi2@studio.unibo.it>
2024-07-11 11:44:23 +0200

    added link coloring

---
+++ app/src/app/dashboard/NavBar.tsx
@@ -0,0 +1,42 @@
+'use client'
+import Link from "next/link"
+import { useSelectedLayoutSegment } from 'next/navigation'
+import { useEffect } from "react"
+
+export default function NavBar() {
+    const segment = useSelectedLayoutSegment()
+    useEffect(() => {
+        const selectedStyle = "bg-blue-300" 
+        const links = Array.from(document.getElementsByClassName("pageLink"))
+        const matches = links.filter(e => e.id == segment)
+        links.forEach(e => e.classList.remove(selectedStyle))
+        if (matches.length != 0) {
+            matches[0]?.classList.add(selectedStyle)
+        } else {
+            document.getElementById("home")?.classList.add(selectedStyle)
+        }
+    })
+
+    return (
+        <nav className="flex flex-col justify-start h-screen bg-white">
+            <div className="h-20 p-2">
+                DB-Prigione
+            </div>
+            <Link href="/dashboard/">
+                <div id="home" className="pageLink p-2">
+                    Home
+                </div>
+            </Link>
+            <Link href="/dashboard/detenuti">
+                <div id="detenuti" className="pageLink p-2">
+                    Detenuti
+                </div>
+            </Link>
+            <Link href="/dashboard/guardie">
+                <div id="guardie" className="pageLink p-2">
+                    Guardie
+                </div>
+            </Link>
+        </nav>
+    )
+}
--- app/src/app/dashboard/layout.tsx
+++ app/src/app/dashboard/layout.tsx
@@ -1,29 +1,5 @@
-import Link from "next/link"
+import NavBar from "./NavBar"
 
-function NavBar() {
-    return (
-        <nav className="flex flex-col justify-start h-screen bg-white">
-            <div className="h-20 p-2">
-                DB-Prigione
-            </div>
-            <Link href="/dashboard/">
-                <div className="p-2">
-                    Home
-                </div>
-            </Link>
-            <Link href="/dashboard/detenuti">
-                <div className="p-2">
-                    Detenuti
-                </div>
-            </Link>
-            <Link href="/dashboard/guardie">
-            <div className="p-2">
-                Guardie
-            </div>
-            </Link>
-        </nav>
-    )
-}
 export default function Layout({
     children, // will be a page or nested layout
   }: {
