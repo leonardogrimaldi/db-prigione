700b349e2755aee70a5fdb08f63a5232915c2783
Leonardo Grimaldi <leonardo.grimaldi2@studio.unibo.it>
2024-07-17 15:21:15 +0200

    update nuovo orario and datatable

--- app/src/app/dashboard/detenuti/DataTable.tsx
+++ app/src/app/dashboard/detenuti/DataTable.tsx
@@ -1,16 +1,21 @@
 import data from "./data.json"
 
+type Detenuto = {
+    id: string,
+    nome: string,
+    cognome: string,
+    inizio: string,
+    fine: string,
+    cella: string,
+    deceduto: boolean
+}
 export default function DataTable() {
-    type myEntry = {
-        one: string,
-        two: string
-    }
-    const entries: myEntry = JSON.parse(JSON.stringify(data))
+    const colnames: string[] = ["id", "Nome", "Cognome", "Inizio", "Fine", "Cella", "Deceduto"]
     return (
         <table className="table-auto w-full bg-white">
             <thead>
                 <tr>
-                    {Object.keys(entries).map(e => {return <th key={e}>{e}</th>})}
+                    {colnames.map(e => {if (e != "id") return <th key={e}>{e}</th>})}
                 </tr>
             </thead>
             <tbody>
--- app/src/app/dashboard/orari/nuovo/page.tsx
+++ app/src/app/dashboard/orari/nuovo/page.tsx
@@ -1,4 +1,18 @@
+import { Fragment } from "react"
+
 export default function NuovoOrarioGuardia() {
+    const ora: Orario = {
+        id: "1",
+        data_inizio: "2024-02-02",
+        data_fine: "2025-03-03",
+        blocco: "A",
+        piano: "3",
+        orario: new Map<string, boolean[]>([
+            ["Mattina", [false, false, true,false, true, true, true]],
+            ["Pomeriggio", [true, false, true,true, false, false, false]],
+            ["Sera", [false, false, false,false, true, false, false]]
+        ])
+    }
     return (
         <form className="bg-blue-50 pt-5 pl-5 rounded-lg w-full">
             <div className="flex flex-col">
@@ -8,7 +22,8 @@ export default function NuovoOrarioGuardia() {
                         <input className="py-2 px-2" value="Mario Rossi" type="text" name="nome" readOnly />
                     </div>
                 </div>
-                <Orari></Orari>
+                <CardOrario {...ora} ></CardOrario>
+                <InputOrario></InputOrario>
                 <div className="p-3 flex gap-x-5 justify-end">
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Invia</button>
                     <button className="cancella bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" type="submit">Cancella</button>
@@ -17,26 +32,83 @@ export default function NuovoOrarioGuardia() {
         </form>
     )
 }
-
-function Orari() {
+type Orario = {
+    id: string,
+    data_inizio: string,
+    data_fine: string,
+    blocco: string,
+    piano: string,
+    orario: Map<string, boolean[]>
+}
+function CardOrario(e: Orario) {
     return (
         <div className="flex flex-col">
+            <div className="p-3">Orario {e.id}</div>
+            <div className="p-3 flex flex-row gap-x-5">
+                <div className="flex flex-col w-1/4">
+                    <div>Data inizio:</div>
+                    <div className="py-2 px-2 bg-white">{e.data_inizio}</div>
+                </div>
+                <div className="flex flex-col w-1/4">
+                    <div>Data fine:</div>
+                    <div className="py-2 px-2 bg-white">{e.data_fine}</div>
+                </div>
+                <div className="flex flex-col w-1/4">
+                    <div>Blocco:</div>
+                    <div className="py-2 px-2 bg-white">{e.blocco}</div>
+                </div>
+                <div className="flex flex-col w-1/4">
+                    <div>Piano:</div>
+                    <div className="py-2 px-2 bg-white">{e.piano}</div>
+                </div>
+            </div>
+            <div className="p-3 grid grid-cols-8 grid-rows-4">
+                <div></div>
+                <div>Lunedì</div>
+                <div>Martedì</div>
+                <div>Mercoledì</div>
+                <div>Giovedì</div>
+                <div>Venerdì</div>
+                <div>Sabato</div>
+                <div>Domenica</div>
+                {
+                    Array.from(e.orario.keys()).map(k => {
+                        return (
+                            <Fragment key={k}>
+                                <div>{k}</div> 
+                                {e.orario.get(k)?.map(e => {
+                                    return e ? <input key={k} type="checkbox" checked disabled/> : <input key={k} type="checkbox" disabled/>
+                                })}
+                            </Fragment>
+                        )
+                    })
+                }
+            </div>
+        </div>
+        
+    )
+}
+
+function InputOrario() {
+    return (
+        <div className="flex flex-col">
+            <div className="p-3">Nuovo orario</div>
             <div className="p-3 flex flex-row gap-x-5">
                 <div className="flex flex-col w-1/4">
                     <label htmlFor="data_inizio">Data inizio:</label>
-                    <input className="py-2 px-2" type="date" name="data_inizio" readOnly/>
+                    <input className="py-2 px-2" type="date" name="data_inizio"/>
                 </div>
                 <div className="flex flex-col w-1/4">
                     <label htmlFor="data_fine">Data fine:</label>
-                    <input className="py-2 px-2" type="date" name="data_fine" readOnly/>
+                    <input className="py-2 px-2" type="date" name="data_fine"/>
                 </div>
                 <div className="flex flex-col w-1/4">
                     <label htmlFor="blocco">Blocco:</label>
-                    <input className="py-2 px-2" type="text" name="blocco" readOnly/>
+                    <input className="py-2 px-2" type="text" name="blocco"/>
                 </div>
                 <div className="flex flex-col w-1/4">
                     <label htmlFor="piano">Piano:</label>
-                    <input className="py-2 px-2" type="text" name="piano" readOnly/>
+                    <input className="py-2 px-2" type="text" name="piano"/>
                 </div>
             </div>
             <div className="p-3 grid grid-cols-8 grid-rows-4">
@@ -49,11 +121,11 @@ function Orari() {
                 <div>Sabato</div>
                 <div>Domenica</div>
                 <div>Mattina</div>
-                {Array.from(Array(7).keys()).map(e => {return <input key={e} type="checkbox" checked disabled/>})}
+                {Array.from(Array(7).keys()).map(e => {return <input key={e} type="checkbox"/>})}
                 <div>Pomeriggio</div>
-                {Array.from(Array(7).keys()).map(e => {return <input key={e} type="checkbox" checked disabled/>})}
+                {Array.from(Array(7).keys()).map(e => {return <input key={e} type="checkbox"/>})}
                 <div>Sera</div>
-                {Array.from(Array(7).keys()).map(e => {return <input key={e} type="checkbox" disabled/>})}
+                {Array.from(Array(7).keys()).map(e => {return <input key={e} type="checkbox"/>})}
             </div>
         </div>
         
